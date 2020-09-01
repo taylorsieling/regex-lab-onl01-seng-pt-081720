1d2e10dd75d599d93665e343c62659bfb80401d1
Taylor Sieling <taylorasieling@gmail.com>
2020-09-01 15:04:35 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,16 +1,26 @@
 def starts_with_a_vowel?(word)
-
+  if word.match (/^[aeiou]|^[AEIOU]/)
+    true 
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan (/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  
+  if text.match(/^[A-Z].*[\.,:;]$/)
+    true 
+  else 
+    false
+  end
 
 end
 
